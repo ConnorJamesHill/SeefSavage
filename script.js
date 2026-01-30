2f6d9ba211354a983ee49c762330f4abed3ea212
Connor Hill <jamesconnorhill@gmail.com>
2026-01-29 22:59:46 -0500

    h

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const CHANNEL_ID = 'UCwqQP1iu8Jpq7ECpJc9lbgg';
 const socialLinks = {
     tiktok: {
         name: 'TikTok',
-        url: 'https://tiktok.com/@YourUsername', // REPLACE with your TikTok URL
+        url: 'https://www.tiktok.com/@seefsavage?_r=1&_t=ZP-93UebW9K0ze',
         icon: `<svg viewBox="0 0 24 24" fill="currentColor">
                 <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1.04-.1z"/>
               </svg>`,
